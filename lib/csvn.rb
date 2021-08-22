b41d7e1cd2119253f3df265049115a9485d26a6b
shamrytski.d <shamrytski.d@profitero.com>
2021-08-22 21:05:45 +0300

    homepage chane in .gemspec

--- lib/csvn.rb
+++ lib/csvn.rb
@@ -1,6 +1,15 @@
 require "csvn/version"
 
-module Csvn
+class Csvn
+  def initialize
+    @a = 1
+    @b = 2
+  end
+
+  def self.super_duper
+
+  end
+
   class Error < StandardError; end
   # Your code goes here...
 end
